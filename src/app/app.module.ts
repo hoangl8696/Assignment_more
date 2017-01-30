fcf586276e0fd1f1b2a53e867fc78f84ce40c14e
hoangl8696 <lyhoang080696@gmail.com>
2017-01-30 13:55:01 +0200

    setting up routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FrontComponent } from './front/front.component';
@@ -9,6 +10,30 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+
+const routeConfig = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/login'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent
+  },
+  {
+    path: 'front',
+    component: FrontComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +47,8 @@ import { LogoutComponent } from './logout/logout.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routeConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
